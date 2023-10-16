ad288ab7af924b7f785c9e8010098d00c7deab80
Yu-1120 <1186893249@qq.com>
2023-10-16 16:48:02 +0800

    update

--- test/ESP32-C3-LuatOS/Esp32C3-Arduino-Clock/.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.d
+++
@@ -1,5 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/FirmwareMSC.cpp.o: \
- C:/Users/11868/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.cpp \
- C:/Users/11868/.platformio/packages/framework-arduinoespressif32/cores/esp32/FirmwareMSC.h \
- C:/Users/11868/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- C:/Users/11868/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32c3/dio_qspi/include/sdkconfig.h
